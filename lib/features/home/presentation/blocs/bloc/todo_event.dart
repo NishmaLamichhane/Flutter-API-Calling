c1aaf18d47ffb3b6390365c1f0b2708c27cb9726
NishmaLamichhane <nishmalamichhane2005@gmail.com>
2025-05-20 09:52:45 +0545

    Update: Add Todo Event

--- lib/features/home/presentation/blocs/bloc/todo_event.dart
+++ lib/features/home/presentation/blocs/bloc/todo_event.dart
@@ -3,3 +3,7 @@ part of 'todo_bloc.dart';
 @immutable
 sealed class TodoEvent {}
 class FetchTodoEvent extends TodoEvent{}
+class AddTodoEvent extends TodoEvent {
+  final Map<String, dynamic> formData;
+  AddTodoEvent({required this.formData});
+}
